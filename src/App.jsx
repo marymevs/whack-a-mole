6b5b70ce2b11961f3fc488c19ef01d7e30833245
Mary Imevbore <mimevbore14@gmail.com>
2026-02-15 16:21:28 -0500

    Revert "Format opening page"
    This reverts commit 1bcfea4e28ec8f1e3baddcc52a22aec29742516f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
 export default function App() {
-  return (
-    <main>
-      <h1>Whack a Mole</h1>
-      <p>Welcome to Whack a Mole!</p>
-      <p>Whack a mole to earn points.</p>
-      <p>How many can you get?</p>
-      <button>Play</button>
-      <h2>High Scores</h2>
-      <p>None yet...Play the game!</p>
-    </main>
-  );
+  return <></>;
 }
