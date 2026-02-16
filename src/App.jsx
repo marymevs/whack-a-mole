c39980fe20461e68dece8679ce5dbdee853418c2
Mary Imevbore <mimevbore14@gmail.com>
2026-02-15 20:51:30 -0500

    Move game title to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,5 +6,10 @@ import { useGame } from "./GameContext";
 export default function App() {
   const { page } = useGame();
 
-  return page === "welcome" ? <Welcome /> : <Game />;
+  return (
+    <>
+      <h1>Whack a Mole</h1>
+      {page === "welcome" ? <Welcome /> : <Game />}
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GameProvider>
-      <h1>Whack a Mole</h1>
       <App />
     </GameProvider>
   </StrictMode>,
